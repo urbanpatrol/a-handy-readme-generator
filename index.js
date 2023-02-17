35d8a13ae15e0f3c39f6c8eee16fa96bb1a77298
urbanpatrol <33529607+urbanpatrol@users.noreply.github.com>
2023-02-17 17:27:47 +0200

    Base files commit including package.json and questions to be utlised n index.js

---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license does your project have?",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "GPL3.0", "BSD2" ,"BSD3", "None"]
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "Are there any dependencies to install?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run for testing?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is this repo used for?",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are the contributors to this repo?",
+    }
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// function to initialize program
+function init() {
+    inquirer.createPromptModule(questions)
+    .then((inquirerAnswers) => {
+        console.log("Generating... Please wait...");
+        writeToFile("./dist/README.md", generateMarkdown({inquirerAnswers}));
+    })
+
+}
+
+// function call to initialize program
+init();
